018fb2eba165ea1fb17276ae0e1ea3cfc785dc31
G r e y <grey@coder.com>
2021-03-23 18:59:08 +0000

    chore: Generate changelog from template
    Resolves: #63

---
+++ scripts/create-clog.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# create-clog.sh <version> <release-date>
+#
+# Dependencies: sed
+# 
+# Description: Uses template_changelog.md to create a new changelog.
+#
+# Examples:
+#  ./scripts/create-clog.sh "1.18.0" "05/18/2021"
+
+
+set -eou pipefail
+
+VERSION_DELIM="<% X.Y.Z %>"
+RELEASE_DATE_DELIM="<% MM/DD/YYYY %>"
+VERSION=""
+RELEASE_DATE=""
+
+# usage prints information for how to use this program
+usage () {
+  echo "Usage: create-clog <version> <release-date>"
+  echo "Create a changelog from a template"
+  echo "Arugments:"
+  echo "    <version>: A version string 'x.y.z'"
+  echo "    <release-date>: A date string 'mm/dd/yyyy'"
+}
+
+# init parses arguments and initializes all variables.
+init () {
+  lenArgs=2
+  if [ "$#" -ne $lenArgs ]; then
+    echo "illegal number of arguments."
+    echo "Expected: $lenArgs  Received: $#"
+    usage
+    exit 1
+  fi
+
+  if [ -z "${1-}" ]; then
+    echo "version argument is empty."
+    echo "Expected a version string like 'x.y.z'"
+    usage
+    exit 1
+  fi
+  
+  if [ -z "${2-}" ]; then
+    echo "release-date argument is empty."
+    echo "Expected a date string like 'mm/dd/yyyy'"
+    usage
+    exit 1
+  fi
+  
+  VERSION="${1}"
+  RELEASE_DATE="${2}"
+}
+
+# createFromTemplate creates a new changelog file from template_changelog.md
+# using VERSION and RELEASE_DATE.
+createFromTemplate () {
+  echo "Creating template using version: ${VERSION} and release date: ${RELEASE_DATE}"
+  pushd "$(git rev-parse --show-toplevel)" > /dev/null
+    clogPath="./changelog/${VERSION}.md"
+    cp ./scripts/template_changelog.md "${clogPath}"
+    sed -i "s|${VERSION_DELIM}|${VERSION}|g" "${clogPath}"
+    sed -i "s|${RELEASE_DATE_DELIM}|${RELEASE_DATE}|g" "${clogPath}"
+  popd > /dev/null
+}
+
+# main program
+init "$@"
+createFromTemplate
+echo "Done"
---
+++ scripts/template_changelog.md
@@ -0,0 +1,20 @@
+---
+title: "<% X.Y.Z %>"
+description: "Released on <% MM/DD/YYYY %>"
+---
+
+### Breaking Changes ❗
+
+There are no breaking changes in <% X.Y.Z %>.
+
+### Features ✨
+
+There are no new features in <% X.Y.Z %>.
+
+### Bug Fixes 🐛
+
+There are no bug fixes in <% X.Y.Z %>.
+
+### Security Updates 🔐
+
+There are no security updates in <% X.Y.Z %>.
