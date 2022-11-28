b938db0d4313ddca82154d3ee5bf2a8647c8efdc
Cian Johnston <cian@coder.com>
2022-11-28 10:42:48 +0000

    chore: add script to validate manifest versions

---
+++ scripts/validate-manifest.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+#
+# validate-manifest.sh [path/to/manifest.json]
+#
+# Dependencies: jq tr sort uniq
+#
+# Description: Ensures consistency of versions specified in manifest.json.
+# This script takes no arguments. Returns 0 if no issues found.
+
+set -eou pipefail
+
+if ! command -v jq > /dev/null; then
+    echo "This script requires jq to be available."
+    exit 1
+fi
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+MANIFEST_JSON_PATH=${1:-"${GIT_ROOT}/manifest.json"}
+
+declare -a MANIFEST_VERSIONS
+declare -a ROUTE_VERSIONS
+
+# Read the versions array in manifest.json
+readarray -t MANIFEST_VERSIONS < <(jq -r '
+    .versions[]
+    | capture("(?<v>[0-9]+\\.[0-9]+)")
+    | .v' < "${MANIFEST_JSON_PATH}")
+
+# Read all the child paths of changelog/index.md and extract major.minor version
+readarray -t ROUTE_VERSIONS < <(jq -r '
+    .routes[]
+    | select(.path == "./changelog/index.md")
+    | .children[]
+    | .path |
+    capture("(?<v>[0-9]+\\.[0-9]+)")
+    | .v' < "${MANIFEST_JSON_PATH}")
+
+# Compare the two
+DIFF=$(echo "${MANIFEST_VERSIONS[@]}" "${ROUTE_VERSIONS[@]}" | tr ' ' '\n' | sort | uniq -u)
+if [[ -n $DIFF ]]; then
+    echo "manifest.json: missing version for changelog(s): ${DIFF}"
+    exit 1
+fi
+
+exit 0
